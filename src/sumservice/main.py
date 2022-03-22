d8ffb32bdd6bdc0a6ba1db0b00f2beb035575ad4
Kozzy Hasebe <hasebe@google.com>
2022-03-23 00:11:45 +0900

    Update the default name of hello_world handler

--- src/sumservice/main.py
+++ src/sumservice/main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Challenger01")
     return "Hello {}!".format(name)
 
 
